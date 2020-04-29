1277fec3476f53123c564ce7c8989c50f734c4ee
Michel Vanderhulst <michelvanderhulst@hotmail.com>
2020-04-29 20:55:51 +0200

    base + Callorput + Spot/K+ Sliders +SL Functions+Maturity + TC 2

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ app.layout = html.Div([
         	step=0.25,
         	updatemode='drag',
         	value=1),
-    	#
+    	
     	html.Div([
         	html.Div([
             	html.Label('Transaction costs'),
